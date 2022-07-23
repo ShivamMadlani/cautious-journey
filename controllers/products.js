61663d3f349b14dd2d9ff3e91b2740706b7ba9a4
Shivam <shivammadlani5@gmail.com>
2022-07-23 14:01:36 +0530

    sort, field, page functionality

--- controllers/products.js
+++ controllers/products.js
@@ -6,7 +6,7 @@ const getAllProdStatic = async (req, res) => {
 }
 
 const getAllProd = async (req, res) => {
-    const { featured, company, name } = req.query
+    const { featured, company, name, sort, fields } = req.query
     const queryObj = {}
     if (featured) {
         queryObj.featured = featured === 'true' ? true : false
@@ -17,7 +17,25 @@ const getAllProd = async (req, res) => {
     if (name) {
         queryObj.name = { $regex: name, $options: 'i' }
     }
-    const products = await Product.find(queryObj)
+    let result = Product.find(queryObj)
+    if (sort) {
+        const sortList = sort.split(',').join(' ')
+        result = result.sort(sortList)
+    }
+    else {
+        result = result.sort('createdOn')
+    }
+    if (fields) {
+        const fieldList = fields.split(',').join(' ')
+        result = result.select(fieldList)
+    }
+
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page - 1) * limit
+    result = result.skip(skip).limit(limit)
+
+    const products = await result
     res.status(200).json({ products })
 }
 
